600cd1a568cf88e68b8cc3e63841b2501b00085e
B-AJ-Amar <amarbouakaz91@gmail.com>
2025-06-10 22:14:35 +0100

    Enhance Author and Book models with validation rules and struct methods

--- bookstore/models/author.go
+++ bookstore/models/author.go
@@ -1,11 +1,23 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
 
 
 type Author struct {
 	Model
-	Name        string  `gorm:"column:name"`
+	Name        string  `gorm:"column:name" validate:"required,maches=^[a-zA-Z ]+$,max=100"` // Name must be alphabetic and max 100 characters
+	Email       string  `gorm:"column:email;unique" validate:"email,max=100"` // Email must be unique, valid email format, and max 100 characters
 	BirthDate   time.Time `gorm:"column:birth_date"`
 	Biography   *string `gorm:"column:biography"`
+	Books       []Book  `gorm:"foreignKey:AuthorID"`
+}
+
+
+func (a *Author) ValidateAuthor() error {
+	validate := validator.New()
+	return validate.Struct(a)
 }
--- bookstore/models/book.go
+++ bookstore/models/book.go
@@ -1,11 +1,21 @@
 package models
 
+import (
+	"github.com/go-playground/validator/v10"
+)
 
 type Book struct {
 	Model
-	// Author      Author	   `gorm:"foreignKey:AuthorID;references:ID"`
-	Name        string     `gorm:"column:name"`
-	Description *string    `gorm:"column:description"`
-	Price       float64    `gorm:"column:price"`
+	AuthorID      int
+	Author      Author   `gorm:"foreignKey:AuthorID";constraint:OnDelete:SET NULL;"`
+	Name        string   `gorm:"column:name" validate:"required,max=100,matches=^[a-zA-Z0-9_ ]*$"` 
+	Description *string  `gorm:"column:description" validate:"max=500"` 
+	Price       float64  `gorm:"column:price" validate:"required,gt=0"` 
 
+}
+
+
+func (b *Book) ValidateBook() error {
+	validate := validator.New()
+	return validate.Struct(b)
 }
